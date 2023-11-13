dcc5d5b9676825fcdff5a74aea63b5426df34b12
yanglinen <mutolee@qq.com>
2023-11-13 19:11:17 +0800

    Add: 商品单规格组件

---
+++ src/components/common/sku-single.vue
@@ -0,0 +1,89 @@
+<script setup>
+import {defineProps} from "vue";
+
+const props = defineProps(['skus'])
+
+// 规格数组，由父节点传进来
+const skus = props.skus
+// 数据结构：
+// [{
+//     name: '',
+//     price: 0.01,
+//     delPrice: 0.02,
+//     count: 99
+// }]
+
+/**
+ * 添加一行
+ */
+const addRow = () => {
+    skus.push({
+        name: '',
+        price: 0.01,
+        delPrice: 0.02,
+        count: 99
+    })
+}
+
+/**
+ * 删除一行
+ * @param index 数组元素下标
+ */
+const delRow = (index) => {
+    // 删除指定下标的元素
+    skus.splice(index, 1)
+}
+
+</script>
+
+<template>
+    <div class="sku_single">
+        <el-row :gutter="10">
+            <el-col :span="8">
+                <el-text>规格名称</el-text>
+            </el-col>
+            <el-col :span="4">
+                <el-text>实际价格</el-text>
+            </el-col>
+            <el-col :span="4">
+                <el-text>划线价格</el-text>
+            </el-col>
+            <el-col :span="4">
+                <el-text>库存</el-text>
+            </el-col>
+            <el-col :span="4">
+                <el-text>操作</el-text>
+            </el-col>
+        </el-row>
+        <el-row :gutter="10" v-for="(item, index) in skus" :key="index">
+            <el-col :span="8">
+                <el-input type="text" v-model="item.name"/>
+            </el-col>
+            <el-col :span="4">
+                <el-input type="number" v-model="item.price"/>
+            </el-col>
+            <el-col :span="4">
+                <el-input type="number" v-model="item.delPrice"/>
+            </el-col>
+            <el-col :span="4">
+                <el-input type="number" v-model="item.count"/>
+            </el-col>
+            <el-col :span="4">
+                <el-button type="danger" icon="Delete" circle @click="delRow(index)" v-if="skus.length > 1"/>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24">
+                <el-button type="primary" @click="addRow" plain>添加一行</el-button>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<style scoped>
+
+.sku_single .el-row {
+    margin-bottom: 10px;
+}
+
+</style>
---
+++ src/views/example/page-sku-single.vue
@@ -0,0 +1,36 @@
+<script setup>
+import SkuSingle from "@/components/common/sku-single.vue";
+import {ref} from "vue";
+
+// 初始化sku
+const skus = ref([
+    // 数据结构：
+    {
+        name: '',
+        price: 0.01,
+        delPrice: 0.02,
+        count: 99
+    }
+])
+
+</script>
+
+<template>
+    <div class="page">
+        <el-card shadow="never" class="vel_card_override">
+            <sku-single :skus="skus"></sku-single>
+            {{skus}}
+        </el-card>
+    </div>
+
+</template>
+
+<style scoped>
+.page {
+    padding: 20px;
+}
+
+.vel_card_override {
+    height: calc(100vh - 90px - 20px - 20px - 2px);
+}
+</style>
